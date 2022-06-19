71d1357045ef57ad4111a20273b76efdf9de7bfd
fernando <f2m25582@gmail.com>
2022-06-19 19:32:44 +0200

    first commit

---
+++ src/main/java/com/ffmoyano/idunn/IdunnApplication.java
@@ -0,0 +1,13 @@
+package com.ffmoyano.idunn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdunnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdunnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ffmoyano/idunn/IdunnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffmoyano.idunn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdunnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
